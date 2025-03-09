ce946c2ed3c27ea77f87634d92bb8249d4f4492c
Pezhwa <pejhvaman@gmail.com>
2025-03-09 16:50:20 +0330

    Fixed: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,50 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname),
-      },
-    ],
-  },
+  entry: path.resolve(__dirname, "src/app.js"), // Make sure this path is correct
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    clean: true,
+    publicPath: "/", // Ensure the correct path is used in dev server
   },
-  devtool: "inline-source-map",
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name][ext]",
+        },
       },
     ],
   },
-  resolve: {
-    extensions: [".js"],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "app.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" }, // Ensure assets are copied properly
+      ],
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"), // Serve static files
+    hot: true,
+    port: 3000,
+    historyApiFallback: true, // Ensures the correct routing for SPA
   },
+  mode: "development",
 };
